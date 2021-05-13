fe05a4649f6593e49c158f254e0484244aea3c89
Leo Rönnebro <leo.ronnebro@nerdyhamster.net>
2021-05-13 15:20:44 +0200

    [Logging] Added logger for exception handler.

--- src/LinuxLudo.API/Middleware/ExceptionHandlerMiddleware.cs
+++ src/LinuxLudo.API/Middleware/ExceptionHandlerMiddleware.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using LinuxLudo.API.Domain.Response;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace LinuxLudo.API.Middleware
 {
@@ -10,21 +11,23 @@ namespace LinuxLudo.API.Middleware
     {
         
         private readonly RequestDelegate _next;
+        private readonly ILoggerFactory _loggerFactory;
 
-        public ExceptionHandlerMiddleware(RequestDelegate next)
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
         {
             _next = next;
+            _loggerFactory = loggerFactory;
         }
 
         public async Task InvokeAsync(HttpContext ctx)
         {
+            var logger = _loggerFactory.CreateLogger<ExceptionHandlerMiddleware>();
             try
             {
-                await _next(ctx);
-            }
+                await _next(ctx); }
             catch (Exception ex)
             {
-                
+                logger.LogError(ex, $"Error: {ex.Message}");
                 var err = new ErrorResponse()
                 {
                     Error = "Server encountered an unexpected error",
