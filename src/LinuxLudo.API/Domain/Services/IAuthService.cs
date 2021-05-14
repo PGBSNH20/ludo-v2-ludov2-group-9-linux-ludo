b1150d60b0f405cc3dfe7da08f98b388954b573f
Leo Rönnebro <leo.ronnebro@nerdyhamster.net>
2021-05-14 22:01:32 +0200

    [Feat] Signup is now working without JWT.

--- src/LinuxLudo.API/Domain/Services/IAuthService.cs
+++ src/LinuxLudo.API/Domain/Services/IAuthService.cs
@@ -1,12 +1,13 @@
 using System.Threading.Tasks;
 using LinuxLudo.API.Domain.Models.Auth;
+using LinuxLudo.API.Domain.Response;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LinuxLudo.API.Domain.Services
 {
     public interface IAuthService
     {
-        Task<string> SignInAsync(User user);
-        Task<string> SingUpAsync(User user);
+        Task<BaseResponse> SignInAsync(User user);
+        Task<BaseResponse> SingUpAsync(User user);
     }
 }
--- src/LinuxLudo.API/Services/AuthService.cs
+++ src/LinuxLudo.API/Services/AuthService.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using LinuxLudo.API.Domain.Models.Auth;
+using LinuxLudo.API.Domain.Response;
 using LinuxLudo.API.Domain.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -16,19 +19,24 @@ namespace LinuxLudo.API.Services
             _userManager = userManager;
         }
 
-        public async Task<string> SignInAsync(User user)
+        public async Task<BaseResponse> SignInAsync(User user)
         {
             throw new System.NotImplementedException();
         }
 
-        public async Task<string> SingUpAsync(User user)
+        public async Task<BaseResponse> SingUpAsync(User user)
         {
             var existing = await _userManager.FindByNameAsync(user.UserName);
 
             if (existing != null)
-                return "User already exists";
+                return new ErrorResponse("User already exists.", 409, null).Respond();
 
-            throw new NotImplementedException();
+            var isCreated = await _userManager.CreateAsync(user);
+
+            if (!isCreated.Succeeded)
+                return new ErrorResponse(isCreated.Errors.Select(e => e.Description).First(), 400, null).Respond();
+
+            return new SuccessResponse("Account created", 201, null).Respond();
         }
     }
 }
