4e99932c8d1ad3f25817f68c3d5d04d439ffdad0
Leo Rönnebro <leo.ronnebro@nerdyhamster.net>
2021-05-23 13:46:09 +0200

    [Controller] Basic GameController

---
+++ src/LinuxLudo.API/Controllers/GamesController.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using LinuxLudo.API.Domain.Resources;
+using LinuxLudo.API.Domain.Response;
+using LinuxLudo.API.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinuxLudo.API.Controllers
+{
+    [Route("/api/[controller]")]
+    public class GamesController : Controller
+    {
+        private readonly IGameService _gameService;
+        private readonly IMapper _mapper;
+
+        public GamesController(IGameService gameService, IMapper mapper)
+        {
+            _gameService = gameService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateGameResource resource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new ErrorResponse(ModelState.Values.First().Errors.First().ErrorMessage, 500, null).Respond());
+
+            return Ok();
+        }
+    }
+}
