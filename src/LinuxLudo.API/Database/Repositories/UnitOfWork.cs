2192e733ce78e7f331de04c5bd3a90b4fba384e4
Leo Rönnebro <leo.ronnebro@nerdyhamster.net>
2021-05-23 13:45:13 +0200

    [UnitOfWork] Basic completed

---
+++ src/LinuxLudo.API/Database/Repositories/UnitOfWork.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using LinuxLudo.API.Database.Context;
+using LinuxLudo.API.Domain.Repositories;
+
+namespace LinuxLudo.API.Database.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _ctx;
+        private GameRepository _gameRepository;
+        private PlayerStatsRepository _playerStatsRepository;
+
+        public UnitOfWork(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IGameRepository Games => _gameRepository = _gameRepository ?? new (_ctx);
+        public IPlayerStatsRepository PlayerStats => _playerStatsRepository = _playerStatsRepository ?? new (_ctx);
+        
+        public void Dispose()
+        {
+            _ctx.Dispose();
+        }
+        
+        public async Task<int> CommitAsync()
+        {
+            return await _ctx.SaveChangesAsync();
+        }
+    }
+}
