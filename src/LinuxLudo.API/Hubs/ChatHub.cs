615cfbbc8554f65dfa3ba8784bcf9230c3887f33
Leo Rönnebro <leo.ronnebro@nerdyhamster.net>
2021-05-24 14:30:54 +0200

    [WebSocket] Basic websocket with signalR

---
+++ src/LinuxLudo.API/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace LinuxLudo.API.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string msg)
+        {
+            await Clients.All.SendAsync("ReceiveMessagesss", user, msg);
+        }
+    }
+}
